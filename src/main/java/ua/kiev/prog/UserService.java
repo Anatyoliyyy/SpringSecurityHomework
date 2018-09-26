dbcaf317be1c67d3cabea0b2693d31035a40652c
Anatyoliyyy <anatoliyyym@ukr.net>
2018-09-26 22:26:22 +0300

    Initial commit

---
+++ src/main/java/ua/kiev/prog/UserService.java
@@ -0,0 +1,13 @@
+package ua.kiev.prog;
+
+import java.util.List;
+
+public interface UserService {
+    CustomUser getUserByLogin(String login);
+    List<CustomUser> getAllUsers();
+    boolean existsByLogin(String login);
+    void addUser(CustomUser customUser);
+    void updateUser(CustomUser customUser);
+    void deleteUser(CustomUser customUser);
+    void deleteUserByLogin(String login);
+}
